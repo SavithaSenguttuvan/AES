b8c03512fa3031467d82724994bbebffe62949e5
savitha <twinsavita@gmail.com>
2017-09-09 02:11:35 +0000

    Added changes to the circular buffer

--- HW/HW1/Circ_Buffer/main.c
+++ HW/HW1/Circ_Buffer/main.c
@@ -14,7 +14,7 @@ int main()
 	uint32_t data = 1;
 	uint32_t removed_data;
 	
-	buffer_status = CB_init(&CircBufferPtr,buffer_length);
+	buffer_status = CB_init(&CircBufferPtr,buffer_length);		/* Initializing the circular buffer with length buffer_length */
 	
 	if(buffer_status == BUFF_NO_ERR)
 	{
@@ -22,7 +22,7 @@ int main()
 	    {
 	        if((CB_is_full(CircBufferPtr)) == BUFF_NO_ERR)
     	    {
-        	    buffer_status = CB_buffer_add_item(CircBufferPtr,data++);
+        	    buffer_status = CB_buffer_add_item(CircBufferPtr,data++);		/* Add items into the buffer */
     	    }
     	    else
             {
@@ -33,14 +33,14 @@ int main()
 	    }
 	    printf("The current number of elements in the buffer is %d\n",CB_size(CircBufferPtr));
 	    printf("Dumping\n");
-	    CB_buff_dump(CircBufferPtr);
+	    CB_buff_dump(CircBufferPtr);											/* Print the values in the buffer */
 	    
 	    no_of_items = 10;
 	    while(no_of_items)
 	    {
 	        if((CB_is_full(CircBufferPtr)) == BUFF_NO_ERR)
     	    {
-        	    buffer_status = CB_buffer_add_item(CircBufferPtr,data++);
+        	    buffer_status = CB_buffer_add_item(CircBufferPtr,data++);		/* Add new items into the buffer */
     	    }
     	    else
             {
@@ -54,7 +54,7 @@ int main()
 	    {
 	        if((CB_is_empty(CircBufferPtr)) == BUFF_NO_ERR)
     	    {
-        	    buffer_status = CB_buffer_remove_item(CircBufferPtr,&removed_data);
+        	    buffer_status = CB_buffer_remove_item(CircBufferPtr,&removed_data);	/*Remove the item from the buffer */
         	    printf("The value is %d\n",removed_data);
         	    
     	    }
